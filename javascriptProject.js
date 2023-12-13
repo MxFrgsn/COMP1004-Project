4872b715b31974c496836f598d33edc2fb18b4a9
Max Ferguson <136943496+MxFrgsn@users.noreply.github.com>
2023-12-13 09:52:46 +0000

    required

--- javascriptProject.js
+++ javascriptProject.js
@@ -2,15 +2,19 @@ var words1 = ["Farm", "Cheese", "Apple", "Character", "Planet", "Godfrey", "Oran
  "Ocassionally", "Always", "Never", "Cow", "Animal", "Dog", "Sheep", "Banana", "Rarely", "However", "Oil", "Defined", "Variable"];
  
 function validateForm() {
-  let x = getElementById["input"].value;
-  for (var i, i<len(words), i++)
+  let x = document.getElementById("inputbox").value;
+  
+  for (var i = 0; i<words1.length;i++)
+  {
 	if (x == words1[i])
 	{
 		alert("Correct!");
 		return true;
+	}
 	else
+	{
 		alert("Please enter word in array: ");
 		return false;
-	
+	}
   }
 }
