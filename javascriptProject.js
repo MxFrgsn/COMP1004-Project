3a6166120e000b78c7f772eee98026cc5a10bc33
Max Ferguson <136943496+MxFrgsn@users.noreply.github.com>
2024-01-14 09:53:36 +0000

    summary

--- javascriptProject.js
+++ javascriptProject.js
@@ -1,22 +1,36 @@
-var list = ["Farm", "Cheese", "Apple", "Character", "Planet", "Godfrey", "Orange", "Story", "Schelude", "Often",
- "Ocassionally", "Always", "Never", "Cow", "Animal", "Dog", "Sheep", "Banana", "Rarely", "However", "Oil", "Defined", "Variable"];
+var expectedResults = ["Farm", "Cheese", "Apple", "Character", "Planet", "Godfrey", "Orange", "Story", "Animated", "User", "Empty", "Still", "Water", "Grapes", "Fill", "When", "Why", "Worry", "Sad", "Pensive", "Because", "Intuition", "Pattern", "Recognition",
+"Oval", "Square", "Paper", "Developer", "Controls", "Button", "Listener", "String", "Data", "Explained", "Error", "Means", "Exist", "Queue", "Event", "Object", "Exuberant", "Terrified", "Grief", "Morning", "Sunny", "Rain", "Vigorously",
+"Rich", "Abundant", "Lush", "Lubricant", "Average", "Mode", "Mind", "Direction", "Rock", "Cartoon", "Stories", "Protagonist", "Anti-Hero", "Robot", "Drone", "King", "History", "Exiled", "Death", "Tortured", "Soul", "Religion",
+"Eagle", "Black", "Blue", "Pigeon", "Parrot", "Extraordinary", "Powerful", "Earth", "Air", "Fire", "Water", "Avatar", "Lazy", "Brilliant", "Genius", "Gas", "Parliament", "Government", "Decree", "Judge", "Jury", "Execute", "Program",
+"Algorithm", "Strong", "Timid", "Assembly", "Require", "Spontaneous", "Tore", "Rip", "Schedule", "Often", "Occasionally", "Always", "Never", "Cow", "Animal", "Dog", "Sheep", "Banana", "Rarely", "However", "Oil", "Defined", "Variable"];
  
+let paragraph = document.createElement("p");
 //colorPicker.addEventListener("input", updateFirst, false);
 
-function validateForm() {
-  let inputbox = document.getElementById("inputbox").value;
-  
-  for (var i = 0; i<list.length;i++)
-  {
-	if (inputbox == list[i])
+function displayExpectedResults()
+{
+	let arrayDisplayElement = document.getElementById("displayArray");
+	for (var i = 0; i< expectedResults.length; i++)
 	{
-		alert("Correct!");
-		return true;
+        paragraph.textContent += expectedResults[i] + " ";
+	}
+	arrayDisplayElement.appendChild(paragraph);
+}
+function validateForm() {
+  event.preventDefault(); 
+  let inputBox = document.getElementById("inputBox").value;
+  
+	if (inputBox == expectedResults[0])
+	{
+		expectedResults.shift(); // want it to glow green when true + remove it from the list on the html doc
+		paragraph.innerHTML = "";
+		displayExpectedResults();
+		inputBox.className = "input.green";
 	}
 	else
 	{
-		alert("Please enter word in array: ");
-		return false;
+		inputBox.className = "input.red";
 	}
   }
-}
+displayExpectedResults();
+ // can input validation for every single letter entered and show it ont he green?
