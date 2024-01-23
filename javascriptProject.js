f41b4233bcc37e368c3462beff5148cf27e6ac00
Max Ferguson <136943496+MxFrgsn@users.noreply.github.com>
2024-01-23 14:12:37 +0000

    Commit

--- javascriptProject.js
+++ javascriptProject.js
@@ -1,14 +1,13 @@
 var expected_Inputs = ["Farm", "Cheese", "Apple", "Character", "Planet", "Godfrey", "Orange", "Story", "Animated", "User", "Empty", "Still", "Water", "Grapes", "Fill", "When", "Why", "Worry", "Sad", "Pensive", "Because", "Intuition", "Pattern", "Recognition",
 "Oval", "Square", "Paper", "Developer", "Controls", "Button", "Listener", "String", "Data", "Explained", "Error", "Means", "Exist", "Queue", "Event", "Object", "Exuberant", "Terrified", "Grief", "Morning", "Sunny", "Rain", "Vigorously",
 "Rich", "Abundant", "Lush", "Lubricant", "Average", "Mode", "Mind", "Direction", "Rock", "Cartoon", "Stories", "Protagonist", "Anti-Hero", "Robot", "Drone", "King", "History", "Exiled", "Death", "Tortured", "Soul", "Religion",
-"Eagle", "Black", "Blue", "Pigeon", "Parrot", "Extraordinary", "Powerful", "Earth", "Air", "Fire", "Water", "Avatar", "Lazy", "Brilliant", "Genius", "Gas", "Parliament", "Government", "Decree", "Judge", "Jury", "Execute", "Program",
-"Algorithm", "Strong", "Timid", "Assembly", "Require", "Spontaneous", "Tore", "Rip", "Schedule", "Often", "Occasionally", "Always", "Never", "Cow", "Animal", "Dog", "Sheep", "Banana", "Rarely", "However", "Oil", "Defined", "Variable"];
- 
-let paragraph = document.createElement("p");
+"Eagle", "Black", "Blue", "Pigeon", "Parrot", "Extraordinary", "Powerful", "Earth", "Air", "Fire"];
+var back_up= ["Water", "Avatar", "Lazy", "Brilliant", "Genius", "Gas", "Parliament", "Government", "Decree", "Judge", "Jury", "Execute", "Program","Algorithm", "Strong", "Timid", "Assembly", "Require", "Spontaneous", "Tore", "Rip", "Schedule", "Often", "Occasionally", "Always", "Never", "Cow", "Animal", "Dog", "Sheep", "Banana", "Rarely", "However", "Oil", "Defined", "Variable"];
 var completed_Words = 0;
 var timerInterval;
 var startTime;
 var interval;
+let paragraph = document.createElement("p");
 
 function displayExpectedInputs()
 {
@@ -28,15 +27,16 @@ function validateForm() {
 	if (input_Box == expected_Inputs[0])
 	{
 		completed_Words++;
-		expected_Inputs.shift(); // want it to glow green when true + remove it from input box
+
+		expected_Inputs.shift();// want it to glow green when true + remove it from input box
+		if (completed_Words%2 == 0 || back_up[0].length <= 7) // want to find a better solution to ensure clean UI is maintained (height needs to be 287)
+		{
+		expected_Inputs.push(back_up[0]);
+		back_up.shift();
+		}
 		paragraph.innerHTML = "";
 		input_Box.value = ""
 		displayExpectedInputs();
-		input_Box.className = "input.green";
-	}
-	else
-	{
-		input_Box.className = "input.red";
 	}
   }
 
@@ -44,14 +44,20 @@ function timer()
 {
 	let current_Time = new Date();
     let time_Passed = current_Time - start_Time;
+	let timer_paragraph  = document.createElement("p");
+	let timerHTML = document.getElementById("timer");
 
     if (time_Passed >= 10000) { // 60000 milliseconds = 60 seconds
-      console.log("It has been ",time_Passed/1000,"seconds."); //changes this so it updates nicely on screen
+      //console.log("It has been ",time_Passed/1000,"seconds."); //changes this so it updates nicely on screen -> in process and not working!!!
+	  paragraph.textContent== time_Passed/1000
 	  clearInterval(timer_Interval);
       calculateWPM()
     } else {
-      console.log("It has been",time_Passed/1000,"seconds.");
+      //console.log("It has been",time_Passed/1000,"seconds.");
+	  paragraph.textContent== time_Passed/1000
+
     }
+	timerHTML.appendChild(timer_paragraph)
 }
 function calculateWPM()
 {
@@ -74,10 +80,6 @@ function timerCallback()
 	console.log("End of Timer");
 }
 
-function selectTime()
-{
-	selected_time 
-}
-
 displayExpectedInputs();
 interval = setInterval(checkIfInputBoxEmpty, 1000);
+
